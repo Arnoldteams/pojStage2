0c55da87d58268bed6941d8e64ea4a9dec8a4914
于艳帆 <raink3@126.com>
2022-06-25 01:47:16 +0000

    删除文件 SSMPoj2

--- SSMPoj2/src/main/java/com/cskaoyan/mapper/AccountMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cskaoyan.mapper;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface AccountMapper {
-    public String queryById(@Param("id") Integer id);
-
-    public void updateById(@Param("id") Integer id, @Param("money") String money);
-}
--- SSMPoj2/src/main/java/com/cskaoyan/service/AccountService.java
+++
@@ -1,5 +0,0 @@
-package com.cskaoyan.service;
-
-public interface AccountService {
-    public void transfer(Integer fromAcc,Integer toAcc,String money);
-}
