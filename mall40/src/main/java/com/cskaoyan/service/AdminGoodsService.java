d37e9b7e700268a29b0d73191323759fec996017
SssdSssd <1733517125@qq.com>
2022-06-26 16:32:55 +0800

    Sssd, 0625, feat, 新增显示商品列表

---
+++ mall40/src/main/java/com/cskaoyan/service/AdminGoodsService.java
@@ -0,0 +1,2 @@
+package com.cskaoyan.service;public interface AdminGoodsService {
+}
---
+++ mall40/src/main/java/com/cskaoyan/service/AdminGoodsServiceImpl.java
@@ -0,0 +1,7 @@
+package com.cskaoyan.service;/**
+ * @description: 系统管理-商品管理
+ * @author: Sssd 
+ * @date: 2022年06月26日 13:47 
+ */
+public class AdminGoodsServiceImpl {
+}
