61e1db0d8b3b687307a5ecb024f766ffa9326ead
13996118712 <1045882503@qq.com>
2022-06-30 22:06:21 +0800

    sprinkle 0630 22:06 feat:增加了修改密码

---
+++ mall40/src/main/java/com/cskaoyan/controller/AdminProfileController.java
@@ -0,0 +1,43 @@
+package com.cskaoyan.controller;
+
+import com.cskaoyan.bean.BasePageInfo;
+import com.cskaoyan.bean.BaseRespVo;
+import com.cskaoyan.bean.MarketOrder;
+import com.cskaoyan.bean.bo.AdminOrderListBO;
+import com.cskaoyan.bean.bo.AdminProfilePasswordBO;
+import com.cskaoyan.bean.param.CommonData;
+import com.cskaoyan.handler.LogAnnotation;
+import com.cskaoyan.service.AdminProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 修改密码
+ *
+ * @author sprinkle
+ * @since 2022/06/30 21:10
+ */
+@RestController
+@RequestMapping("admin/profile")
+public class AdminProfileController {
+
+    @Autowired
+    AdminProfileService adminProfileService;
+
+    @LogAnnotation(value = "修改密码")
+    @PostMapping ("password")
+    public BaseRespVo list(@RequestBody@Validated AdminProfilePasswordBO admin, BindingResult bindingResult) {
+        String errMsg = adminProfileService.updatePassword(admin);
+
+        if(errMsg == null){
+            return BaseRespVo.invalidAuth("账户密码不对");
+        }
+
+        return BaseRespVo.ok();
+    }
+}
