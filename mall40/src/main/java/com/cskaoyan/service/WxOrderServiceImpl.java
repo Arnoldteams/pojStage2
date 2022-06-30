d13ac14cc753260485a5217046106a5025960e50
SparkX <xyg2597@163.com>
2022-06-30 22:38:37 +0800

    sparkx,0630,fix:修复小程序首页显示错误，后台专题添加id未显示错误

--- mall40/src/main/java/com/cskaoyan/service/WxOrderServiceImpl.java
+++ mall40/src/main/java/com/cskaoyan/service/WxOrderServiceImpl.java
@@ -15,6 +15,7 @@ import com.github.pagehelper.PageInfo;
 import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
