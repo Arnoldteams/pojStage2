3dd560a5dc9ff6c2ded7e0f281f6a7f8348df33a
guan-ziyue <1170294895@qq.com>
2022-07-11 08:50:54 +0800

    7.11

--- Poject3/duolaimall/duolaimall-order/order-service/src/main/java/com/cskaoyan/order/service/impl/OrderQueryServiceImpl.java
+++ Poject3/duolaimall/duolaimall-order/order-service/src/main/java/com/cskaoyan/order/service/impl/OrderQueryServiceImpl.java
@@ -53,11 +53,15 @@ public class OrderQueryServiceImpl implements OrderQueryService {
 
         try {
             request.requestCheck();
-            PageHelper.startPage(request.getPage(), request.getSize(), request.getSort());
             // 获取该用户的所有订单
+            PageHelper.startPage(request.getPage(), request.getSize(), request.getSort());
             Example example = new Example(Order.class);
             example.createCriteria().andEqualTo("userId", request.getUserId());
             List<Order> orders = orderMapper.selectByExample(example);
+
+            // 分页处理
+            PageInfo<Order> pageInfo = new PageInfo<>(orders);
+
             List<OrderDetailInfo> orderDetailInfos = converter.order2detail(orders);
 
             for (OrderDetailInfo orderDetailInfo : orderDetailInfos) {
@@ -76,11 +80,8 @@ public class OrderQueryServiceImpl implements OrderQueryService {
                 orderDetailInfo.setOrderShippingDto(orderShippingDto);
             }
 
-            // 分页处理
-            PageInfo<OrderDetailInfo> pageInfo = new PageInfo<>(orderDetailInfos);
-            int size = pageInfo.getSize();
-            response.setDetailInfoList(pageInfo.getList());
-            response.setTotal((long) size);
+            response.setDetailInfoList(orderDetailInfos);
+            response.setTotal(pageInfo.getTotal());
             response.setCode(SysRetCodeConstants.SUCCESS.getCode());
             response.setMsg(SysRetCodeConstants.SUCCESS.getMessage());
         }catch (Exception e){
